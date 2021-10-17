a470256fb357f22324362abd9523ba1c21d96774
Dlimingliang <lml2dyy@gmail.com>
2021-10-17 09:03:10 +0800

    寻找偶数个数

--- src/main/java/com/lml/algorithmlearn/Array/Array.java
+++ src/main/java/com/lml/algorithmlearn/Array/Array.java
@@ -29,18 +29,23 @@ public class Array {
 
     public static int findEvenNumberCount(int[] nums) {
 
+        //时间复杂度 O(n)
+        //空间复杂度 O(1)
         int count = 0, length = nums.length;
-
         for (int i = 0; i < length; i++) {
 
-            String.valueOf(nums[i]).length();
+            if (String.valueOf(nums[i]).length() % 2 == 0) {
+                count ++;
+            }
         }
         return count;
     }
 
     public static void main(String[] args) {
 
-        int[] array = new int[]{1,1,1,0,1};
-        System.out.println(findMaxConsecutiveOnes(array));
+//        int[] array = new int[]{1,1,1,0,1};
+//        System.out.println(findMaxConsecutiveOnes(array));
+        int[] evenNumberArray = new int[] {12,345,2,6,7896};
+        System.out.println(findEvenNumberCount(evenNumberArray));
     }
 }
