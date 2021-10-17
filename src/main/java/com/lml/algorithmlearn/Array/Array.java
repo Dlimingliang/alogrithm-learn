3e4b0553da16ec69441ca93d527a3e68cf096491
Dlimingliang <lml2dyy@gmail.com>
2021-10-17 10:30:51 +0800

    双指针

--- src/main/java/com/lml/algorithmlearn/Array/Array.java
+++ src/main/java/com/lml/algorithmlearn/Array/Array.java
@@ -1,5 +1,7 @@
 package com.lml.algorithmlearn.Array;
 
+import java.util.Arrays;
+
 /**
  * @Auther: limingliang
  * @Description:
@@ -41,11 +43,87 @@ public class Array {
         return count;
     }
 
+    public static int[] squareAndSortArray(int[] nums) {
+
+        //时间复杂度 O(n log n)
+        //空间复杂度 O(n)
+        int [] ans = new int[nums.length];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            ans[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+
+    public static int[] squareAndSortArrayByTwoPoint(int[] nums) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        int length = nums.length;
+        int negative = -1;
+        for (int i = 0; i < length; i++) {
+
+            if (nums[i] < 0) {
+                negative = i;
+            } else {
+                break;
+            }
+        }
+
+        int [] ans = new int[length];
+        int index = 0, i = negative, j = negative + 1;
+
+        while (i >= 0 || j < length) {
+
+            if (i < 0) {
+                ans[index] = nums[j] * nums[j];
+                j++;
+            } else if(j == length) {
+
+                ans[index] = nums[i] * nums[i];
+                i--;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+
+                ans[index] = nums[i] * nums[i];
+                i--;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                j++;
+            }
+            index++;
+        }
+        return ans;
+    }
+
+    public static int[] squareAndSortArrayByTwoPointBetter(int[] nums) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        int length = nums.length;
+        int [] ans = new int[length];
+        int i = 0, j = length -1, index = length -1;
+        while (i <= j) {
+
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                i++;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                j--;
+            }
+            index--;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
-        int[] array = new int[]{1,1,1,0,1};
-        System.out.println(findMaxConsecutiveOnes(array));
-        int[] evenNumberArray = new int[] {12,345,2,6,7896};
-        System.out.println(findEvenNumberCount(evenNumberArray));
+//        int[] array = new int[]{1,1,1,0,1};
+//        System.out.println(findMaxConsecutiveOnes(array));
+//        int[] evenNumberArray = new int[] {12,345,2,6,7896};
+//        System.out.println(findEvenNumberCount(evenNumberArray));
+        int [] squareArray = new int[]{-5,-3,-2,-1};
+        squareAndSortArrayByTwoPoint(squareArray);
     }
 }
