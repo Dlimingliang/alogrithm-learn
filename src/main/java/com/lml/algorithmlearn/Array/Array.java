616c9c4fc4c7d551474578145857cacff053d363
Dlimingliang <lml2dyy@gmail.com>
2021-10-17 09:15:08 +0800

    寻找偶数个数

--- src/main/java/com/lml/algorithmlearn/Array/Array.java
+++ src/main/java/com/lml/algorithmlearn/Array/Array.java
@@ -43,8 +43,8 @@ public class Array {
 
     public static void main(String[] args) {
 
-//        int[] array = new int[]{1,1,1,0,1};
-//        System.out.println(findMaxConsecutiveOnes(array));
+        int[] array = new int[]{1,1,1,0,1};
+        System.out.println(findMaxConsecutiveOnes(array));
         int[] evenNumberArray = new int[] {12,345,2,6,7896};
         System.out.println(findEvenNumberCount(evenNumberArray));
     }
