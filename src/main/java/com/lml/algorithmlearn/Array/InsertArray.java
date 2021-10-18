8bf746dc6b79e73c5a433f2eb1d0da1476be7316
Dlimingliang <lml2dyy@gmail.com>
2021-10-18 13:16:20 +0800

    利用计算将双循环降低为单训话，时间复杂度大大降低

--- src/main/java/com/lml/algorithmlearn/Array/InsertArray.java
+++ src/main/java/com/lml/algorithmlearn/Array/InsertArray.java
@@ -65,9 +65,36 @@ public class InsertArray {
         System.out.println(arr);
     }
 
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        for (int i = m + n - 1; i >= 0; i--) {
+
+            if (m <= 0 && n > 0) {
+                nums1[i] = nums2[n - 1];
+                n--;
+            } else if (m > 0 && n <= 0) {
+                nums1[i] = nums1[m - 1];
+                m--;
+            } else if (m > 0 && n > 0) {
+                if (nums1[m - 1] > nums2[n - 1]) {
+                    nums1[i] = nums1[m - 1];
+                    m--;
+                } else {
+                    nums1[i] = nums2[n - 1];
+                    n--;
+                }
+            }
+        }
+
+        System.out.println(nums1);
+    }
+
     public static void main(String[] args) {
 
-        int[] array = new int[]{8,4,5,0,0,0,0,7};
-        duplicateZerosBetter(array);
+//        int[] array = new int[]{8,4,5,0,0,0,0,7};
+//        duplicateZerosBetter(array);
+        merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
     }
 }
