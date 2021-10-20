cb7529e90fec44354af1791668c7fb8c3f5058aa
Dlimingliang <lml2dyy@gmail.com>
2021-10-20 15:16:19 +0800

    查找元素

--- src/main/java/com/lml/algorithmlearn/Array/SearchArray.java
+++ src/main/java/com/lml/algorithmlearn/Array/SearchArray.java
@@ -49,11 +49,39 @@ public class SearchArray {
         return false;
     }
 
+    public static boolean validMountainArray(int[] arr) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        int N = arr.length;
+        int i = 0;
+
+        // 递增扫描
+        while (i + 1 < N && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        // 最高点不能是数组的第一个位置或最后一个位置
+        if (i == 0 || i == N - 1) {
+            return false;
+        }
+
+        // 递减扫描
+        while (i + 1 < N && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == N - 1;
+
+    }
+
 
 
     public static void main(String[] args) {
 
-        int[] array = new int[]{3,1,14,11};
-        System.out.println(checkIfExistBetter(array));
+//        int[] array = new int[]{3,1,14,11};
+//        System.out.println(checkIfExistBetter(array));
+        int[] MountainArray = new int[]{0,3,2,1};
+        System.out.println(validMountainArray(MountainArray));
     }
 }
