6172f046b5a796c7d4823da092bf6107ab18fbb0
Dlimingliang <lml2dyy@gmail.com>
2021-10-27 10:31:15 +0800

    测试提交

--- src/main/java/com/lml/algorithmlearn/linkedList/RandomLinkedList.java
+++ src/main/java/com/lml/algorithmlearn/linkedList/RandomLinkedList.java
@@ -1,5 +1,8 @@
 package com.lml.algorithmlearn.linkedList;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Auther: limingliang
  * @Description:
@@ -10,6 +13,7 @@ public class RandomLinkedList {
 
     class Node {
         int val;
+        int randomIndex;
         Node next;
         Node random;
 
@@ -20,8 +24,55 @@ public class RandomLinkedList {
         }
     }
 
+    Map<Node, Node> cachedNode = new HashMap<Node, Node>();
+
     public Node copyRandomList(Node head) {
 
-        return head;
+        // 时间复杂度O(n)
+        // 空间复杂度O(n)
+        if (head == null) {
+            return null;
+        }
+
+        if (!cachedNode.containsKey(head)) {
+
+            Node headNew = new Node(head.val);
+            cachedNode.put(head, headNew);
+            headNew.next = copyRandomList(head.next);
+            headNew.random = copyRandomList(head.random);
+        }
+        return cachedNode.get(head);
+    }
+
+    public Node copyRandomListBetter(Node head) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(n)
+        if (head == null) {
+            return null;
+        }
+
+        //复制新节点到后面
+        for(Node node = head; node != null ; node = node.next.next) {
+
+            Node newNode = new Node(node.val);
+            newNode.next = node.next;
+            node.next = newNode;
+        }
+
+        //复制随机节点
+        for(Node node = head; node != null ; node = node.next.next) {
+            Node nodeNew = node.next;
+            nodeNew.random = (node.random != null) ? node.random.next : null;
+        }
+
+        //去除老节点
+        Node headNew = head.next;
+        for (Node node = head; node != null; node = node.next) {
+            Node nodeNew = node.next;
+            node.next = node.next.next;
+            nodeNew.next = (nodeNew.next != null) ? nodeNew.next.next : null;
+        }
+        return headNew;
     }
 }
