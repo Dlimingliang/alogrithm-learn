99ff237bf6e3fa27c09c3f312823f16ea81b629d
limingliang <lml2dyy@gmail.com>
2021-11-03 17:23:37 +0800

    hash表设计

--- src/main/java/com/lml/algorithmlearn/hash/HashMapLearn.java
+++ src/main/java/com/lml/algorithmlearn/hash/HashMapLearn.java
@@ -3,7 +3,9 @@ package com.lml.algorithmlearn.hash;
 import com.lml.algorithmlearn.array.Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HashMapLearn {
@@ -122,12 +124,63 @@ public class HashMapLearn {
 //        return -1;
     }
 
+    public static int[] intersect(int[] nums1, int[] nums2) {
+
+        //时间复杂度O(n + m)
+        //空间复杂度O(n)
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] array = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            array[i] = result.get(i);
+        }
+        return array;
+
+        //时间复杂度 O(mlogm+nlogn)
+        //空间复杂度 O()
+//        Arrays.sort(nums1);
+//        Arrays.sort(nums2);
+//        int length1 = nums1.length, length2 = nums2.length;
+//        int[] intersection = new int[Math.min(length1, length2)];
+//        int index1 = 0, index2 = 0, index = 0;
+//        while (index1 < length1 && index2 < length2) {
+//            if (nums1[index1] < nums2[index2]) {
+//                index1++;
+//            } else if (nums1[index1] > nums2[index2]) {
+//                index2++;
+//            } else {
+//                intersection[index] = nums1[index1];
+//                index1++;
+//                index2++;
+//                index++;
+//            }
+//        }
+//        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+//    public boolean containsNearbyDuplicate(int[] nums, int k) {
+//
+//    }
+
     public static void main(String[] args) {
 
 //        System.out.println(isIsomorphic("paper", "title"));
 //        String[] list1 = new String[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
 //        String[] list2 = new String[]{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"};
 //        System.out.println(findRestaurant(list1, list2));
-        System.out.println(firstUniqChar("aadadaad"));
+//        System.out.println(firstUniqChar("aadadaad"));
+        int[] array = new int[]{1,2,2,1};
+        int[] array2 = new int[]{1,1};
+        System.out.println(intersect(array, array2));
     }
 }
