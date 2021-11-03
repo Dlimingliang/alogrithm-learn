20365e47de16b4bb87b2790cba49c4cb894d6448
limingliang <lml2dyy@gmail.com>
2021-11-03 15:54:46 +0800

    hash表设计

--- src/main/java/com/lml/algorithmlearn/hash/HashMapLearn.java
+++ src/main/java/com/lml/algorithmlearn/hash/HashMapLearn.java
@@ -1,5 +1,8 @@
 package com.lml.algorithmlearn.hash;
 
+import com.lml.algorithmlearn.array.Array;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,8 +54,40 @@ public class HashMapLearn {
         return true;
     }
 
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+
+        //时间复杂度O(l1 + l2)
+        //空间复杂度O(l1 * x)
+        HashMap<String, Integer> map = new HashMap();
+        int minIndex = Integer.MAX_VALUE, num = 0;
+        ArrayList<String> stringArrayList = new ArrayList();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        for (int i = 0; i < list2.length; i++) {
+            if (map.containsKey(list2[i])) {
+                num = i + map.get(list2[i]);
+                if (num < minIndex) {
+                    stringArrayList.clear();
+                    minIndex = num;
+                    stringArrayList.add(list2[i]);
+                } else if (num == minIndex) {
+                    stringArrayList.add(list2[i]);
+                }
+            }
+
+        }
+        return stringArrayList.toArray(new String[stringArrayList.size()]);
+    }
+
+
+
     public static void main(String[] args) {
 
-        System.out.println(isIsomorphic("paper", "title"));
+//        System.out.println(isIsomorphic("paper", "title"));
+        String[] list1 = new String[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] list2 = new String[]{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"};
+        System.out.println(findRestaurant(list1, list2));
     }
 }
