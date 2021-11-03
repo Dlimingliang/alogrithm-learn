992a49a0d4aaba9d89b5f28a1699f6a5dd9489d8
limingliang <lml2dyy@gmail.com>
2021-11-03 12:34:08 +0800

    hash表设计

---
+++ src/main/java/com/lml/algorithmlearn/hash/HashMapLearn.java
@@ -0,0 +1,44 @@
+package com.lml.algorithmlearn.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapLearn {
+
+    public int[] twoSumLoop(int[] nums, int target) {
+
+        //时间复杂度O(n^2)
+        //空间复杂度O(1)
+        int[] result = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        //时间复杂度o(n)
+        //空间复杂度O(n)
+        //第一遍放入值
+        int length = nums.length;
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
