32f7c2d966196d90e0a2277785fa156dd7f0fbfd
limingliang <lml2dyy@gmail.com>
2021-11-05 09:12:30 +0800

    hash表设计

--- src/main/java/com/lml/algorithmlearn/array/ArrayLearn.java
+++ src/main/java/com/lml/algorithmlearn/array/ArrayLearn.java
@@ -1,32 +1,88 @@
 package com.lml.algorithmlearn.array;
 
+import java.util.Arrays;
+
 public class ArrayLearn {
 
     public static int pivotIndex(int[] nums) {
 
         //时间复杂度O(n^2)
         //空间复杂度O(1)
-        int length = nums.length, left, right;
+//        int length = nums.length, left, right;
+//        for (int i = 0; i < length; i++) {
+//
+//            left = 0; right = 0;
+//            for (int j = 0; j < i; j++) {
+//                left += nums[j];
+//            }
+//
+//            for (int j = i + 1; j < length; j++) {
+//                right += nums[j];
+//            }
+//            if (left == right) {
+//                return i;
+//            }
+//        }
+//        return -1;
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        int length = nums.length, sum = 0;
+        int total = Arrays.stream(nums).sum();
         for (int i = 0; i < length; i++) {
-
-            left = 0; right = 0;
-            for (int j = 0; j < i; j++) {
-                left += nums[j];
-            }
-
-            for (int j = i + 1; j < length; j++) {
-                right += nums[j];
-            }
-            if (left == right) {
+            if (total - nums[i] == 2 * sum) {
                 return i;
             }
+            sum += nums[i];
         }
         return -1;
     }
 
+    public static int searchInsert(int[] nums, int target) {
+
+        //时间复杂度O(logn)
+        //空间复杂度O(1)
+//        int end = nums.length, middle = end / 2, start = 0;
+//        if (nums[0] > target) {
+//            return 0;
+//        }
+//        if (nums[end - 1] < target) {
+//            return end;
+//        }
+//        while (true) {
+//            if (nums[middle] == target) {
+//                return middle;
+//            }
+//            if (nums[middle] < target && nums[middle + 1] > target) {
+//                return middle + 1;
+//            }
+//            if (nums[middle] > target) {
+//                end = middle;
+//                middle -= (middle - start) / 2 > 0 ? (middle - start) / 2 : 1;
+//            } else {
+//                start = middle;
+//                middle +=  (end - middle) / 2 > 0 ? (end - middle) / 2 : 1;
+//            }
+//        }
+
+        int length = nums.length;
+        int left = 0, right = length -1, result= length;
+        while (left <= right) {
+            int mid = ((right -left) >> 1) + left;
+            if (target <= nums[mid]) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
-        int[] array = new int[]{2, 1, -1};
-        System.out.println(pivotIndex(array));
+//        int[] array = new int[]{2, 1, -1};
+//        System.out.println(pivotIndex(array));
+        int[] array = new int[] {1,3,5,6};
+        System.out.println(searchInsert(array, 0));
     }
 }
