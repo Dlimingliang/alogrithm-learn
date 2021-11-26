b45c4e0165ddb4f767a9b8e1e9a631ac623c4fab
limingliang <lml2dyy@gmail.com>
2021-11-26 11:58:56 +0800

    二插搜索树的插入

--- src/main/java/com/lml/algorithmlearn/binaryTree/BinarySearchTree.java
+++ src/main/java/com/lml/algorithmlearn/binaryTree/BinarySearchTree.java
@@ -163,6 +163,46 @@ public class BinarySearchTree {
         return result;
     }
 
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        //时间复杂度O(h)
+        //空间复杂度O(n)
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                root = null;
+            }else if (root.right != null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            } else {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+
+    public int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+
+    public int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+
+
+
 
     public static void main(String[] args) {
         TreeNode leftLeft = new TreeNode(1);
