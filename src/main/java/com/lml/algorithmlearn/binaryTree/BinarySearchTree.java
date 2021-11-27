d1ea4f458fcb031a5d8f1a2c699ce4003e9a2d50
limingliang <lml2dyy@gmail.com>
2021-11-27 11:59:33 +0800

    公共子节点

--- src/main/java/com/lml/algorithmlearn/binaryTree/BinarySearchTree.java
+++ src/main/java/com/lml/algorithmlearn/binaryTree/BinarySearchTree.java
@@ -274,6 +274,21 @@ public class BinarySearchTree {
         }
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        while (root != null) {
+
+            if ((p.val < root.val && q.val > root.val) || (p.val > root.val && q.val < root.val) || p.val == root.val || q.val == root.val) {
+                return root;
+            } else if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            }
+        }
+        return root;
+    }
+
 
     public static void main(String[] args) {
 
