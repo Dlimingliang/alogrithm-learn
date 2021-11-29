4815ddf3eb77893e8485d73240d7bf77b3fe24b2
limingliang <lml2dyy@gmail.com>
2021-11-29 11:16:43 +0800

    公共子节点

--- src/main/java/com/lml/algorithmlearn/binaryTree/BalanceBinarySearchTree.java
+++ src/main/java/com/lml/algorithmlearn/binaryTree/BalanceBinarySearchTree.java
@@ -23,14 +23,44 @@ public class BalanceBinarySearchTree {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(logn)
+        return sortedArrayToBSTRecursion(nums, 0, nums.length);
+    }
+
+    private TreeNode sortedArrayToBSTRecursion(int[] nums, int left , int right) {
+
+//        if (right - left == 1) {
+//            return new TreeNode(nums[left]);
+//        }
+//        int num = right - left;
+//        int mid = num % 2 == 0 ? num/2 - 1 : num / 2;
+//        TreeNode node = new TreeNode(nums[left + mid]);
+//        if (mid == 0) {
+//            node.right = new TreeNode(nums[right - 1]);
+//        } else {
+//            node.left = this.sortedArrayToBSTRecursion(nums, left, left + mid);
+//            node.right = this.sortedArrayToBSTRecursion(nums, left + mid + 1, right);
+//        }
+//        return node;
+        if (left > right) {
+            return null;
+        }
+        int mid = (right + left + 1) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = this.sortedArrayToBSTRecursion(nums, left, mid - 1);
+        node.right = this.sortedArrayToBSTRecursion(nums, left + mid + 1, right);
+        return node;
+    }
+
     public static void main(String[] args) {
 
-        TreeNode left = new TreeNode(9);
-        TreeNode rightLeftleft = new TreeNode(13);
-        TreeNode rightLeft = new TreeNode(15, rightLeftleft, null);
-        TreeNode right = new TreeNode(20, rightLeft, null);
-        TreeNode root = new TreeNode(3, left, right);
-        System.out.println(new BalanceBinarySearchTree().isBalanced(root));
+//        int[] array = new int[]{1,3};
+        int[] array = new int[]{0,1,2,3,4,5};
+        TreeNode treeNode = new BalanceBinarySearchTree().sortedArrayToBST(array);
+        System.out.println(treeNode.val);
     }
 
     public static class TreeNode {
