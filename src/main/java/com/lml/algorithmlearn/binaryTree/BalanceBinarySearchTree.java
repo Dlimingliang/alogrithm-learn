ca822c8d4c2a112ff0d4edf41023e6137fa97cf3
limingliang <lml2dyy@gmail.com>
2021-11-29 09:46:58 +0800

    公共子节点

---
+++ src/main/java/com/lml/algorithmlearn/binaryTree/BalanceBinarySearchTree.java
@@ -0,0 +1,46 @@
+package com.lml.algorithmlearn.binaryTree;
+
+public class BalanceBinarySearchTree {
+
+    boolean result = true;
+    public boolean isBalanced(TreeNode root) {
+
+        height(root);
+        return result;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            result = false;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode left = new TreeNode(9);
+        TreeNode rightLeftleft = new TreeNode(13);
+        TreeNode rightLeft = new TreeNode(15, rightLeftleft, null);
+        TreeNode right = new TreeNode(20, rightLeft, null);
+        TreeNode root = new TreeNode(3, left, right);
+        System.out.println(new BalanceBinarySearchTree().isBalanced(root));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
