18224425b60966e9ee1f5cdac2ab82665898b072
limingliang <lml2dyy@gmail.com>
2021-11-30 13:47:11 +0800

    公共子节点

--- src/main/java/com/lml/algorithmlearn/queue/QueueProblem.java
+++ src/main/java/com/lml/algorithmlearn/queue/QueueProblem.java
@@ -15,37 +15,38 @@ public class QueueProblem {
         int nc = grid[0].length;
         int num_islands = 0;
 
-        for (int r = 0; r < nr; r++) {
-            for (int c = 0; c < nc; c++) {
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
                 if (grid[r][c] == '1') {
                     ++num_islands;
                     grid[r][c] = '0';
-                    Queue<Integer> queue = new LinkedList<>();
-                    queue.add(r * nc + c);
-                    while (!queue.isEmpty()) {
-                        int id = queue.remove();
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
                         int row = id / nc;
                         int col = id % nc;
-                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
-                            queue.add((row -1) * nc + c);
-                            grid[row - 1][col] = '0';
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
                         }
-                        if (row + 1 < nr && grid[row + 1][col] == '1') {
-                            queue.add((row + 1) * nc + c);
-                            grid[row + 1][col] = '0';
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
                         }
-                        if (col + 1 < nc && grid[row][col + 1] == '1') {
-                            queue.add(row * nc + c + 1);
-                            grid[row][col + 1] = '0';
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
                         }
-                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
-                            queue.add(row * nc + c - 1);
-                            grid[row][col - 1] = '0';
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
                         }
                     }
                 }
             }
         }
+
         return num_islands;
     }
 
