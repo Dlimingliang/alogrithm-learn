f8410cce54720bfe04ad91bc9d7fa6ab72998d7e
limingliang <lml2dyy@gmail.com>
2021-12-01 10:13:10 +0800

    完全平方数

---
+++ src/main/java/com/lml/algorithmlearn/stack/MinStack.java
@@ -0,0 +1,46 @@
+package com.lml.algorithmlearn.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
