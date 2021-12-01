dff83f8378c236b4f8c583c073f62e0a13025489
limingliang <lml2dyy@gmail.com>
2021-12-01 14:02:49 +0800

    温度

--- src/main/java/com/lml/algorithmlearn/stack/StackLearn.java
+++ src/main/java/com/lml/algorithmlearn/stack/StackLearn.java
@@ -27,24 +27,25 @@ public class StackLearn {
         return deque.isEmpty();
     }
 
-    public static void main(String[] args) {
-        // 1. Initialize a stack.
-        Deque<Integer> s = new LinkedList<>();
-        // 2. Push new element.
-        s.push(5);
-        s.push(13);
-        s.push(8);
-        s.push(6);
-        // 3. Check if stack is empty.
-        if (s.isEmpty()) {
-            System.out.println("Stack is empty!");
-            return;
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int length = temperatures.length;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+
+            int temp = 0;
+            for (int j = i + 1; j < length; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    temp = j - i;
+                    break;
+                }
+            }
+            result[i] = temp;
         }
-        // 4. Pop an element.
-        s.pop();
-        // 5. Get the top element.
-        System.out.println("The top element is: " + s.peek());
-        // 6. Get the size of the stack.
-        System.out.println("The size is: " + s.size());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new StackLearn().dailyTemperatures(new int[]{30,40,50,60}));
     }
 }
