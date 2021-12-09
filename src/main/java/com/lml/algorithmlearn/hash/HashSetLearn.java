7529ece48c85c507bb71b7498fb7c98579249c6c
limingliang <lml2dyy@gmail.com>
2021-12-09 17:11:40 +0800

    无重复最长子串

--- src/main/java/com/lml/algorithmlearn/hash/HashSetLearn.java
+++ src/main/java/com/lml/algorithmlearn/hash/HashSetLearn.java
@@ -1,9 +1,11 @@
 package com.lml.algorithmlearn.hash;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 public class HashSetLearn {
@@ -203,8 +205,39 @@ public class HashSetLearn {
         return result;
     }
 
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(new HashSetLearn().fourSumCount(new int[] {1,2}, new int[]{-2,-1}, new int[]{-1,2}, new int[]{0,2}));
+        System.out.println(new HashSetLearn().topKFrequent(new int[]{1}, 1));
     }
 }
