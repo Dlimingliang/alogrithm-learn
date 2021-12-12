5afd6e7233f71a9e95f0dc64ad5707835078934e
limingliang <lml2dyy@gmail.com>
2021-12-12 10:59:45 +0800

    字符串

---
+++ src/main/java/com/lml/algorithmlearn/recursion/RecursionLearn.java
@@ -0,0 +1,21 @@
+package com.lml.algorithmlearn.recursion;
+
+public class RecursionLearn {
+
+    public void reverseString(char[] s) {
+
+        //时间复杂度O(n)
+        //空间复杂度O(1)
+        int length = s.length;
+        int left = 0, right = length - 1;
+        while (left < right) {
+
+            char temp = s[left];
+            s[left]= s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(s);
+    }
+}
