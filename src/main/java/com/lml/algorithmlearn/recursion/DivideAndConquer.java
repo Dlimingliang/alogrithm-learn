8a0a54ddb96675cb182be10d0143bd6612fcc012
limingliang <lml2dyy@gmail.com>
2021-12-14 16:29:46 +0800

    归并排序

--- src/main/java/com/lml/algorithmlearn/recursion/DivideAndConquer.java
+++ src/main/java/com/lml/algorithmlearn/recursion/DivideAndConquer.java
@@ -103,6 +103,49 @@ public class DivideAndConquer {
         return leftResult && rightResult;
     }
 
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        return searchMatrixHelper(matrix, 0, 0, matrix[0].length - 1, matrix.length - 1, matrix[0].length - 1, matrix.length - 1, target);
+    }
+
+    private boolean searchMatrixHelper(int[][] matrix, int x1, int y1, int x2, int y2, int xMax, int yMax, int target) {
+
+        if (x1 > xMax || y1 > yMax) {
+            return false;
+        }
+
+        if (x1 == x2 && y1 == y2) {
+            return matrix[y1][x1] == target;
+        }
+
+        int m1 = (x1 + x2) / 2;
+        int m2 = (y1 + y2) / 2;
+        if (matrix[m2][m1] == target) {
+            return true;
+        }
+
+        if (matrix[m2][m1] < target) {
+            //抛开左上矩阵
+            return searchMatrixHelper(matrix, m1 + 1, y1, x2, m2, x2, y2, target) ||
+                    // 左下矩阵
+                    searchMatrixHelper(matrix, x1, m2 + 1, m1, y2, x2, y2, target) ||
+                    // 右下矩阵
+                    searchMatrixHelper(matrix, m1 + 1, m2 + 1, x2, y2, x2, y2, target);
+        } else {
+            //抛开右下矩阵
+            // 右上矩阵
+            return searchMatrixHelper(matrix, m1 + 1, y1, x2, m2, x2, y2, target) ||
+                    // 左下矩阵
+                    searchMatrixHelper(matrix, x1, m2 + 1, m1, y2, x2, y2, target) ||
+                    // 左上矩阵
+                    searchMatrixHelper(matrix, x1, y1, m1, m2, x2, y2, target);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new DivideAndConquer().sortArrayBottomUp(new int[] {1,4,5,6,7,8,2,3})));
     }
