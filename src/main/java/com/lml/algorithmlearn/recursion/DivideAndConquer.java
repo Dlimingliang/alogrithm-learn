14ffc13dedb6233c9c7395ef5d6c79850c8c17b3
limingliang <lml2dyy@gmail.com>
2021-12-15 10:01:46 +0800

    归并排序

--- src/main/java/com/lml/algorithmlearn/recursion/DivideAndConquer.java
+++ src/main/java/com/lml/algorithmlearn/recursion/DivideAndConquer.java
@@ -215,8 +215,43 @@ public class DivideAndConquer {
         return result;
     }
 
+    public void quickSortBetter(int [] lst) {
+        /* Sorts an array in the ascending order in O(n log n) time */
+        int n = lst.length;
+        qSort(lst, 0, n - 1);
+        System.out.println(Arrays.toString(lst));
+    }
+
+    private void qSort(int [] lst, int lo, int hi) {
+        if (lo < hi) {
+            int p = partition(lst, lo, hi);
+            qSort(lst, lo, p - 1);
+            qSort(lst, p + 1, hi);
+        }
+    }
+
+    private int partition(int [] lst, int lo, int hi) {
+    /*
+      Picks the last element hi as a pivot
+      and returns the index of pivot value in the sorted array */
+        int pivot = lst[hi];
+        int i = lo;
+        for (int j = lo; j < hi; ++j) {
+            if (lst[j] < pivot) {
+                int tmp = lst[i];
+                lst[i] = lst[j];
+                lst[j] = tmp;
+                i++;
+            }
+        }
+        int tmp = lst[i];
+        lst[i] = lst[hi];
+        lst[hi] = tmp;
+        return i;
+    }
+
     public static void main(String[] args) {
-        new DivideAndConquer().quickSort(new int[]{1,5,3,2,8,7,6,4});
+        new DivideAndConquer().quickSortBetter(new int[]{1,5,3,2,8,7,6,4});
     }
     
     public class TreeNode {
