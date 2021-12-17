e2daaf789c18097e88234a9edfb2304dba9453cb
limingliang <lml2dyy@gmail.com>
2021-12-17 09:51:10 +0800

    数独问题

---
+++ src/main/java/com/lml/algorithmlearn/recursion/RecursionToLoop.java
@@ -0,0 +1,30 @@
+package com.lml.algorithmlearn.recursion;
+
+public class RecursionToLoop {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
