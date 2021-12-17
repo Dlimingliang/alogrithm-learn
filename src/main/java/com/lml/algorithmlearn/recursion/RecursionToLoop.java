9d5f70d51102c57baa1f075ed06871014b50b671
limingliang <lml2dyy@gmail.com>
2021-12-17 10:17:21 +0800

    数独问题

--- src/main/java/com/lml/algorithmlearn/recursion/RecursionToLoop.java
+++ src/main/java/com/lml/algorithmlearn/recursion/RecursionToLoop.java
@@ -1,9 +1,15 @@
 package com.lml.algorithmlearn.recursion;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class RecursionToLoop {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
+        //n为节点数, h为树的高度
+        //时间复杂度 O(n)
+        //空间复杂度 O(h)
         if (p == null && q == null) {
             return true;
         }
@@ -14,6 +20,33 @@ public class RecursionToLoop {
 
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+
+    public boolean isSameTreeLoop(TreeNode p, TreeNode q) {
+
+        boolean result = true;
+        Deque<TreeNode> pQueen = new LinkedList<>();
+        Deque<TreeNode> qQueen = new LinkedList<>();
+        pQueen.add(p);
+        qQueen.add(q);
+        while (!pQueen.isEmpty() && result) {
+
+            TreeNode pNode = pQueen.removeFirst();
+            TreeNode qNode = qQueen.removeFirst();
+            if (pNode == null && qNode == null) {
+                continue;
+            }
+            if (pNode == null || qNode == null || pNode.val != qNode.val) {
+                result = false;
+                continue;
+            }
+            pQueen.add(pNode.left);
+            pQueen.add(pNode.right);
+            qQueen.add(qNode.left);
+            qQueen.add(qNode.right);
+        }
+        return result;
+    }
+
 
     public class TreeNode {
         int val;
