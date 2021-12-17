bb2841edaf80a43528475d77619bd0d8be30ebf4
limingliang <lml2dyy@gmail.com>
2021-12-17 13:44:26 +0800

    数独问题

--- src/main/java/com/lml/algorithmlearn/recursion/RecursionToLoop.java
+++ src/main/java/com/lml/algorithmlearn/recursion/RecursionToLoop.java
@@ -1,5 +1,6 @@
 package com.lml.algorithmlearn.recursion;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -85,6 +86,29 @@ public class RecursionToLoop {
             }
         }
         return result;
+    }
+
+    public List<String> generateParenthesisBack(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
     }
 
     public static void main(String[] args) {
