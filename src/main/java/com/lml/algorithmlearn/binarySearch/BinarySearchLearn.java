b811731f1e4c89ea56ec4fa73b353879c662dfd3
limingliang <lml2dyy@gmail.com>
2021-12-21 09:14:21 +0800

    数独问题

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
@@ -22,7 +22,26 @@ public class BinarySearchLearn {
         return -1;
     }
 
+    public int mySqrt(int x) {
+
+        //时间复杂度O(logx)
+        //空间复杂度O(1)
+        int left = 0, right = x, result = -1;
+        while (left <= right) {
+
+            int mid = (right - left) / 2 + left;
+            long num = (long)mid * (long)mid;
+            if (num <= x) {
+                result = mid;
+                left = mid + 1;
+            } else if (num > x) {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new BinarySearchLearn().search(new int[]{-1,0,3,5,9,12}, 2));
+        System.out.println(new BinarySearchLearn().mySqrt(2147395599));
     }
 }
