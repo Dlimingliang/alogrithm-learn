685ddb8d71b9dc4440b617ea32acc17207e7342b
limingliang <lml2dyy@gmail.com>
2021-12-21 10:46:02 +0800

    二分查找

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
@@ -104,7 +104,29 @@ public class BinarySearchLearn {
 //        return -1;
     }
 
+    public int firstBadVersion(int n) {
+
+        int left = 1, right = n;
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int n) {
+        if (n >= 1) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new BinarySearchLearn().searchA(new int[]{4,5,6,7,8,1,2,3}, 8));
+        System.out.println(new BinarySearchLearn().firstBadVersion(2));
     }
 }
