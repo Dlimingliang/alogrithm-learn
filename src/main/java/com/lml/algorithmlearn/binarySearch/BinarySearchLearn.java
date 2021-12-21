bfd36eacf717e9251c8b97ba9b1f1bd0ec3176a9
limingliang <lml2dyy@gmail.com>
2021-12-21 11:01:48 +0800

    二分查找

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
@@ -126,7 +126,24 @@ public class BinarySearchLearn {
         return false;
     }
 
+    public int findPeakElement(int[] nums) {
+
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if ((mid == 0 || nums[mid - 1] < nums[mid]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1])) {
+                return mid;
+            } else if ((mid == = || nums[mid - 1] < nums[mid]) && (mid == nums.length - 1 || nums[mid] < nums[mid + 1])) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new BinarySearchLearn().firstBadVersion(2));
+        System.out.println(new BinarySearchLearn().findPeakElement(new int[]{1,2}));
     }
 }
