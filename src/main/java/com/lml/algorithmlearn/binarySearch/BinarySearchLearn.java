7b1193829e19f46fd363572a5703314467c9e310
limingliang <lml2dyy@gmail.com>
2021-12-21 13:32:31 +0800

    二分查找

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn.java
@@ -180,6 +180,29 @@ public class BinarySearchLearn {
 //        return nums[left];
     }
 
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(new BinarySearchLearn().findMin(new int[]{11,13,15,17}));
     }
