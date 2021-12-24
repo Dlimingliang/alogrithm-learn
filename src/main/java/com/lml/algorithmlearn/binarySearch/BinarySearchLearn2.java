ccb54ba4f5036ac186794fff23201c32918e7b72
limingliang <lml2dyy@gmail.com>
2021-12-24 08:52:50 +0800

    二分查找

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn2.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn2.java
@@ -77,7 +77,24 @@ public class BinarySearchLearn2 {
         return letters[left % letters.length];
     }
 
+    public int findMin(int[] nums) {
+
+        int left = 0, right = nums.length;
+        while (left < right) {
+
+            int mid = left + (right -  left) / 2;
+            int midNum = nums[mid];
+            if (midNum > nums[nums.length - 1]) {
+
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
     public static void main(String[] args) {
-        System.out.println(new BinarySearchLearn2().nextGreatestLetter(new char[]{'e', 'e', 'n', 'n'}, 'n'));
+        System.out.println(new BinarySearchLearn2().findMin(new int[]{2, 1}));
     }
 }
