1d26dd590447a3f49415eb81cca08a824ce91fcd
limingliang <lml2dyy@gmail.com>
2021-12-24 10:45:11 +0800

    二分查找

--- src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn2.java
+++ src/main/java/com/lml/algorithmlearn/binarySearch/BinarySearchLearn2.java
@@ -162,21 +162,43 @@ public class BinarySearchLearn2 {
 //        }
 //        return -1;
 
-        //快慢指针 时间复杂度O(n) 空间复杂度O(1)
-        int slow = 0, fast = 0;
-        while (true) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            if (slow == fast) {
-                break;
+        //二分查找 时间复杂度O(nlogn) 空间复杂度O(1)
+        int n = nums.length;
+        int left = 1, right = n - 1, ans = -1;
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
             }
+            if (cnt <= mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                ans = mid;
+            }
+
         }
-        slow = 0;
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
+        return ans;
+
+//        //快慢指针 时间复杂度O(n) 空间复杂度O(1)
+//        int slow = 0, fast = 0;
+//        while (true) {
+//            slow = nums[slow];
+//            fast = nums[nums[fast]];
+//            if (slow == fast) {
+//                break;
+//            }
+//        }
+//        slow = 0;
+//        while (slow != fast) {
+//            slow = nums[slow];
+//            fast = nums[fast];
+//        }
+//        return slow;
     }
 
     public static void main(String[] args) {
