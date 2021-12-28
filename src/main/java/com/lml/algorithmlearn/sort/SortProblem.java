52c641210150b483c3a91ce2e588fb0902541e34
limingliang <lml2dyy@gmail.com>
2021-12-28 09:30:41 +0800

    堆排序

--- src/main/java/com/lml/algorithmlearn/sort/SortProblem.java
+++ src/main/java/com/lml/algorithmlearn/sort/SortProblem.java
@@ -1,8 +1,5 @@
 package com.lml.algorithmlearn.sort;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class SortProblem {
 
     public void moveZeroes(int[] nums) {
@@ -56,29 +53,66 @@ public class SortProblem {
 //            nums[i] = minKey;
 //        }
 //        return nums[k - 1];
-        //滑动窗口方案
-        //时间复杂度O(n)
-        //空间复杂度O(k)
-        int length = nums.length, index = 0;
-        Queue<Integer> queue = new PriorityQueue<>(k);
-        while (index < length) {
 
-            if (queue.size() < k) {
-                queue.offer(nums[index]);
-                index++;
-                continue;
-            }
-            if (nums[index] > queue.peek()) {
-                queue.poll();
-                queue.offer(nums[index]);
-            }
-            index++;
+//        //滑动窗口方案
+//        //时间复杂度O(n)
+//        //空间复杂度O(k)
+//        int length = nums.length, index = 0;
+//        Queue<Integer> queue = new PriorityQueue<>(k);
+//        while (index < length) {
+//
+//            if (queue.size() < k) {
+//                queue.offer(nums[index]);
+//                index++;
+//                continue;
+//            }
+//            if (nums[index] > queue.peek()) {
+//                queue.poll();
+//                queue.offer(nums[index]);
+//            }
+//            index++;
+//        }
+//        return queue.poll();
+
+        //堆排序方案
+        for (int i = nums.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(nums, i, nums.length);
         }
-        return queue.poll();
+
+        for (int i = nums.length - 1; i >= nums.length - k + 1; i--) {
+            swap(nums, 0, i);
+            adjustHeap(nums, 0, i);
+        }
+        return nums[0];
+    }
+
+    private void adjustHeap(int[] nums, int i, int length) {
+
+        int temp = nums[i];
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
+
+            if (k + 1 < length && nums[k + 1] > nums[k]) {
+                k++;
+            }
+            if (nums[k] > temp) {
+                nums[i] = nums[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        nums[i] = temp;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3,2,1,5,6,4};
+        int[] nums = new int[]{2,1};
         System.out.println(new SortProblem().findKthLargest(nums, 2));
     }
 }
