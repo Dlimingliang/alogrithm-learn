aa875738fb818e2927acee07a4c922f2af891e51
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-03-16 09:57:53 +0800

    桶排序

---
+++ src/main/java/com/lml/algorithmlearn/array/Array.java
@@ -0,0 +1,25 @@
+package com.lml.algorithmlearn.array;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 0, 1, 1, 1, 1};
+        int result = findMaxConsecutiveOnes(nums);
+        System.out.println(result);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, current = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                current = 0;
+            } else {
+                current++;
+                if (current > max) {
+                    max = current;
+                }
+            }
+        }
+        return max;
+    }
+}
