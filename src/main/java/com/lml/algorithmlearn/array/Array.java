6fcca8c9596926b0e947e89b4ec25b99d32d3ee4
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-03-17 17:36:32 +0800

    桶排序

--- src/main/java/com/lml/algorithmlearn/array/Array.java
+++ src/main/java/com/lml/algorithmlearn/array/Array.java
@@ -3,9 +3,32 @@ package com.lml.algorithmlearn.array;
 public class Array {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 0, 1, 1, 1, 1};
-        int result = findMaxConsecutiveOnes(nums);
-        System.out.println(result);
+        int[] nums = new int[]{-7,-3,2,3,11};
+//        int result = findMaxConsecutiveOnes(nums);
+//        System.out.println(result);
+        sortedSquares(nums);
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        //时间复杂度 O(n)
+        //空间复杂度 O(1)
+        int[] result = new int[nums.length];
+        int left = 0, right = nums.length - 1;
+        int leftSquare, rightSquare, resultIndex = right;
+        while (left <= right) {
+            leftSquare = nums[left] * nums[left];
+            rightSquare = nums[right] * nums[right];
+            if (leftSquare < rightSquare) {
+                result[resultIndex] = rightSquare;
+                right--;
+            } else {
+                result[resultIndex] = leftSquare;
+                left++;
+            }
+            resultIndex--;
+        }
+        return result;
     }
 
     public static int findNumbers(int[] nums) {
