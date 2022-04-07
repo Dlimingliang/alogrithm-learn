6c20c5a32fce26819911d41586e3f966d25b34b5
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-04-07 10:02:55 +0800

    algorithm learn

--- src/main/java/com/lml/algorithmlearn/array/Array.java
+++ src/main/java/com/lml/algorithmlearn/array/Array.java
@@ -18,6 +18,23 @@ public class Array {
 //        duplicateZeros(nums);
 //        sortArrayByParity(new int[]{3,1,2,4});
 //        System.out.println(thirdMax(new int[]{3, 2, 1, 2, 2}));
+        generate(5);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
     }
 
     public void rotate(int[][] matrix) {
