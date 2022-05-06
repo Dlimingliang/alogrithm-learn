e19de9b6e04b2bd6dd9f4a0db106ea6f2da60ab9
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-05-06 08:41:16 +0800

    algorithm learn

--- src/main/java/com/lml/algorithmlearn/linkedList/MySinglyLinkedList.java
+++ src/main/java/com/lml/algorithmlearn/linkedList/MySinglyLinkedList.java
@@ -7,6 +7,7 @@ public class MySinglyLinkedList {
 
     public MySinglyLinkedList() {
         size = 0;
+        head = new SinglyListNode(0);
     }
 
     public int get(int index) {
@@ -14,67 +15,45 @@ public class MySinglyLinkedList {
             return  -1;
         }
         SinglyListNode temp = head;
-        for (int i = 1; i <= index; i++) {
+        for (int i = 0; i <= index; i++) {
             temp = temp.next;
         }
         return temp.val;
     }
 
     public void addAtHead(int val) {
-        SinglyListNode newHead = new SinglyListNode(val);
-        newHead.next = head;
-        head = newHead;
-        size++;
+        addAtIndex(0, val);
     }
 
     public void addAtTail(int val) {
-        if (size == 0) {
-            addAtHead(val);
-        } else {
-            SinglyListNode newHead = new SinglyListNode(val);
-            //获取最后的节点。指向新节点
-            SinglyListNode temp = head;
-            for (int i = 1; i < size; i++) {
-                temp = temp.next;
-            }
-            temp.next = newHead;
-            size++;
-        }
+        addAtIndex(size, val);
     }
 
     public void addAtIndex(int index, int val) {
         if (index < 0 || index > size) {
             return;
         }
-        if (index == 0) {
-            addAtHead(val);
-        } else if (index == size) {
-            addAtTail(val);
-        } else {
-            SinglyListNode newHead = new SinglyListNode(val);
-            SinglyListNode temp = head;
-            for (int i = 1; i < index; i++) {
-                temp = temp.next;
-            }
-            newHead.next = temp.next;
-            temp.next = newHead;
-            size++;
+
+        SinglyListNode newHead = new SinglyListNode(val);
+        SinglyListNode temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
         }
+        newHead.next = temp.next;
+        temp.next = newHead;
+        size++;
     }
 
     public void deleteAtIndex(int index) {
         if (index < 0 || index >= size) {
             return;
         }
-        if (index == 0) {
-            head = head.next;
-        } else {
-            SinglyListNode temp = head;
-            for (int i = 1; i < index; i++) {
-                temp = temp.next;
-            }
-            temp.next = temp.next.next;
+
+        SinglyListNode temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
         }
+        temp.next = temp.next.next;
         size--;
     }
 
